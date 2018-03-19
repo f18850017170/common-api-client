34383bc8dbb60156ecd4a2a2e5920ade6b85829b
Von.诺伊德 <864914320@qq.com>
2018-03-19 15:27:48 +0800

    启用https配置自定义 TrustManager 绕过证书检查

--- src/main/java/cn/vonfly/common/api/client/utils/HttpClientFactory.java
+++ src/main/java/cn/vonfly/common/api/client/utils/HttpClientFactory.java
@@ -1,9 +1,26 @@
 package cn.vonfly.common.api.client.utils;
 
+import org.apache.commons.codec.Charsets;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 public class HttpClientFactory {
     private static volatile CloseableHttpClient httpClient = null;
@@ -22,6 +39,50 @@ public class HttpClientFactory {
                     config.setConnectionRequestTimeout(10000);
                     HttpClientBuilder builder = HttpClientBuilder.create();
                     builder.setDefaultRequestConfig(config.build());
+                    SSLContext sslContext = null;
+                    try {
+                        TrustManager[] trustManagers = new TrustManager[]{
+                                new X509TrustManager() {
+                                    /**
+                                     * 该方法体为空时信任所有客户端证书
+                                     * @param x509Certificates
+                                     * @param s
+                                     * @throws CertificateException
+                                     */
+                                    @Override
+                                    public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+                                    }
+
+                                    /**
+                                     * 该方法体为空时信任所有服务器证书
+                                     * @param x509Certificates
+                                     * @param s
+                                     * @throws CertificateException
+                                     */
+                                    @Override
+                                    public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+                                    }
+
+                                    /**
+                                     * 返回信任的证书
+                                     * @return
+                                     */
+                                    @Override
+                                    public X509Certificate[] getAcceptedIssuers() {
+                                        return null;
+                                    }
+                                }
+                        };
+                        sslContext = SSLContext.getInstance("TLS");
+                        sslContext.init(null, trustManagers, new SecureRandom());
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                            .<ConnectionSocketFactory>register("https", new SSLConnectionSocketFactory(sslContext))
+                            .<ConnectionSocketFactory>register("http",new PlainConnectionSocketFactory())
+                            .build();
                     PoolingHttpClientConnectionManager pools = new PoolingHttpClientConnectionManager();
                     pools.setDefaultMaxPerRoute(20);
                     pools.setMaxTotal(200);
@@ -31,5 +92,16 @@ public class HttpClientFactory {
             }
         }
         return httpClient;
+    }
+
+    public static void main(String[] args) {
+        CloseableHttpClient httpClient = HttpClientFactory.getHttpClient();
+        try {
+            CloseableHttpResponse response = httpClient.execute(new HttpGet("http://www.baidu.com"));
+            String result = EntityUtils.toString(response.getEntity(), Charsets.UTF_8);
+            System.out.println(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
