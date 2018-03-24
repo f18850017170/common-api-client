c392f5069768bea1d5d745a31f2781aece16a99b
Von.诺伊德 <864914320@qq.com>
2018-03-24 17:52:45 +0800

    通过反射获取泛型类型（不用实现重复实现responseClass()） 修复多层级继承时，无法正确获取泛型配置的问题(溯源知道当前抽象类)

--- src/main/java/cn/vonfly/common/api/client/dto/AbsReqSignDto.java
+++ src/main/java/cn/vonfly/common/api/client/dto/AbsReqSignDto.java
@@ -5,12 +5,13 @@ import cn.vonfly.common.api.client.http.HttpPostExecute;
 import cn.vonfly.common.api.client.sign.SignDataBackFill;
 import cn.vonfly.common.api.client.sign.SignResultBackFill;
 import cn.vonfly.common.api.client.sign.annotation.SignIgnore;
+import org.apache.http.util.Asserts;
 
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
-public abstract class AbsReqSignDto<R extends Serializable> extends AbsBaseSignDto implements SignDataBackFill,SignResultBackFill,HttpPostExecute {
+public abstract class AbsReqSignDto<R extends Serializable> extends AbsBaseSignDto implements SignDataBackFill, SignResultBackFill, HttpPostExecute {
     @SignIgnore
     private String sign;
     @SignIgnore
@@ -19,15 +20,22 @@ public abstract class AbsReqSignDto<R extends Serializable> extends AbsBaseSignD
 
     /**
      * 构建请求地址
+     *
      * @return
      */
     public abstract String buildUrl();
+
     /**
      * 返回对象的class
+     *
      * @return
      */
-    public  Class<R> responseClass(){
+    public Class<R> responseClass() {
         Type genericSuperclass = getClass().getGenericSuperclass();
+        while (!(genericSuperclass instanceof ParameterizedType) && !genericSuperclass.equals(AbsReqSignDto.class)) {
+            genericSuperclass = ((Class) genericSuperclass).getGenericSuperclass();
+        }
+        Asserts.check(genericSuperclass instanceof ParameterizedType, "AbsHttpRequest实现类，未指定泛型<R extends Serializable>的真实类型");
         ParameterizedType parameterizedType = (ParameterizedType) genericSuperclass;
         Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
         return (Class<R>) actualTypeArguments[0];
@@ -35,11 +43,13 @@ public abstract class AbsReqSignDto<R extends Serializable> extends AbsBaseSignD
 
     /**
      * 构建失败请求
+     *
      * @param code
      * @param msg
      * @return
      */
-    public abstract R fail(String code,String msg);
+    public abstract R fail(String code, String msg);
+
     @Override
     public void setSign(String sign) {
         this.sign = sign;
